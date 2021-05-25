c5b44495987b1635ed363fa4539ff40e82d7cf10
Luis Costilla <luis287@gmail.com>
2021-05-25 13:13:43 -0400

    Added Restaurant class and controller. Added Migrations

---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    //this is an entity similar to POCO
+    //also there is no need for empty constructor, it is implied
+    public class Restaurant
+    {
+        //Id is required and unique!!!
+        //make sure to have the using statement from above
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+
+        public bool IsRecommended => Rating > 3.5; // this is shorthand
+    }
+}
